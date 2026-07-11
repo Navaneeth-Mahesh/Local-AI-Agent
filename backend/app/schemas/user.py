52462433a184574516494f4866e01057ad4e41d9
Navaneeth <guraninavaneeth@gmail.com>
2026-07-11 21:19:48 +0530

    updated user.py with UserLogin, UserResponse, Token, TokenPayload and also updated doc notes.

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,6 +1,26 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 class UserRegister(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    
+class TokenPayload(BaseModel):
+    sub: str
